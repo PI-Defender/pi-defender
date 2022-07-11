aa27141b0a25ce1ba38db4e429067bf2abf66301
Nikj-Fr <62078072+Nikj-Fr@users.noreply.github.com>
2022-07-11 09:42:30 +0200

    Add files via upload

---
+++ Doc/html/_u___signature_8hpp.js
@@ -0,0 +1,4 @@
+var _u___signature_8hpp =
+[
+    [ "U_SIGNATURE", "class_u___s_i_g_n_a_t_u_r_e.html", "class_u___s_i_g_n_a_t_u_r_e" ]
+];
---
+++ Doc/html/class_k___c_a_c_h_e.js
@@ -0,0 +1,15 @@
+var class_k___c_a_c_h_e =
+[
+    [ "_CACHELIST", "struct_k___c_a_c_h_e_1_1___c_a_c_h_e_l_i_s_t.html", "struct_k___c_a_c_h_e_1_1___c_a_c_h_e_l_i_s_t" ],
+    [ "_HASH", "struct_k___c_a_c_h_e_1_1___h_a_s_h.html", "struct_k___c_a_c_h_e_1_1___h_a_s_h" ],
+    [ "CACHELIST", "class_k___c_a_c_h_e.html#a9c6851583d9132bb819e4ff6c790a057", null ],
+    [ "HASH", "class_k___c_a_c_h_e.html#a16b5f4b02fe2a84314f1c43ac83496e3", null ],
+    [ "PCACHELIST", "class_k___c_a_c_h_e.html#a1430c7fbfbac2310462a41c415ee8d3f", null ],
+    [ "PHASH", "class_k___c_a_c_h_e.html#a5a6beeeba03cd7d1440bf7ac9ea35b6f", null ],
+    [ "_GetCachedValue", "class_k___c_a_c_h_e.html#a2a803e7f4af2e18b582ccc4f2b9f747a", null ],
+    [ "Add", "class_k___c_a_c_h_e.html#a785a988fa6c22e5a7626afe75bd447e3", null ],
+    [ "GetCachedValue", "class_k___c_a_c_h_e.html#a98344c274506318cf85edd7debb5de80", null ],
+    [ "Initialize", "class_k___c_a_c_h_e.html#a85480c0051eb20e2188f5ff40d3debee", null ],
+    [ "_Cache", "class_k___c_a_c_h_e.html#a884d8e7dee5e87f644f2df9d94fac5f5", null ],
+    [ "_pMutex", "class_k___c_a_c_h_e.html#a1e50e86c337a0c7868c6fd992ab1b8b1", null ]
+];
