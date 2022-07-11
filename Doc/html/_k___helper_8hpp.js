c5af933f1cc2aae301f7b29c93eaa35d00da59b7
Nikj-Fr <62078072+Nikj-Fr@users.noreply.github.com>
2022-07-11 09:38:14 +0200

    Add files via upload

---
+++ Doc/html/_k___helper_8hpp.js
@@ -0,0 +1,6 @@
+var _k___helper_8hpp =
+[
+    [ "K_HELPER", "class_k___h_e_l_p_e_r.html", "class_k___h_e_l_p_e_r" ],
+    [ "FILE_BUFFER_SIZE", "_k___helper_8hpp.html#a09dbbd73a84cf772b421c6024b65b1fd", null ],
+    [ "FILE_MAX_SIZE", "_k___helper_8hpp.html#a6747e3e41ec24a747fabfbaad54a202c", null ]
+];
---
+++ Doc/html/_u___communication_8hpp.js
@@ -0,0 +1,8 @@
+var _u___communication_8hpp =
+[
+    [ "U_COMMUNICATION", "class_u___c_o_m_m_u_n_i_c_a_t_i_o_n.html", "class_u___c_o_m_m_u_n_i_c_a_t_i_o_n" ],
+    [ "U_COMMUNICATION::_MESSAGE", "struct_u___c_o_m_m_u_n_i_c_a_t_i_o_n_1_1___m_e_s_s_a_g_e.html", "struct_u___c_o_m_m_u_n_i_c_a_t_i_o_n_1_1___m_e_s_s_a_g_e" ],
+    [ "U_COMMUNICATION::_THREAD_PARAMETERS", "struct_u___c_o_m_m_u_n_i_c_a_t_i_o_n_1_1___t_h_r_e_a_d___p_a_r_a_m_e_t_e_r_s.html", "struct_u___c_o_m_m_u_n_i_c_a_t_i_o_n_1_1___t_h_r_e_a_d___p_a_r_a_m_e_t_e_r_s" ],
+    [ "U_COMMUNICATION::_REPLY_MESSAGE", "struct_u___c_o_m_m_u_n_i_c_a_t_i_o_n_1_1___r_e_p_l_y___m_e_s_s_a_g_e.html", "struct_u___c_o_m_m_u_n_i_c_a_t_i_o_n_1_1___r_e_p_l_y___m_e_s_s_a_g_e" ],
+    [ "REPLY_SIZE", "_u___communication_8hpp.html#aca1f16fcad9c957a3d546a435f6a4f12", null ]
+];
