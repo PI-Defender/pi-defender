cf09e782a9b52eae0078cd740afcf23eae77231e
Nikj-Fr <62078072+Nikj-Fr@users.noreply.github.com>
2022-07-11 09:41:05 +0200

    Add files via upload

---
+++ Doc/html/_u___service_8hpp.js
@@ -0,0 +1,16 @@
+var _u___service_8hpp =
+[
+    [ "_sSERVICE", "struct__s_s_e_r_v_i_c_e.html", "struct__s_s_e_r_v_i_c_e" ],
+    [ "DLL_NAME", "_u___service_8hpp.html#a190dac1af2c221d4be5761c24a63b956", null ],
+    [ "DRIVER_NAME", "_u___service_8hpp.html#a25634d21648ca7fb7a2aca614bafaaeb", null ],
+    [ "DRIVER_SYS", "_u___service_8hpp.html#a7b0c9315f6a4f9d206a1a25413969d34", null ],
+    [ "SERVICE_NAME", "_u___service_8hpp.html#a5ecc06b4fbeedd8edd0692663fb76489", null ],
+    [ "sSERVICE", "_u___service_8hpp.html#a7f93b4efbe1b055ad47a105f35847058", null ],
+    [ "Delete", "_u___service_8hpp.html#a1d7c8f0ec682b654c05c04ba5fcb6e12", null ],
+    [ "Install", "_u___service_8hpp.html#ab5c4ad56c0967c7aa59e84c28c445bc6", null ],
+    [ "Query", "_u___service_8hpp.html#aff39ead9760e0687df750416a070f137", null ],
+    [ "Service", "_u___service_8hpp.html#afa2e3daf278842db7a49d41ebebc8519", null ],
+    [ "Start", "_u___service_8hpp.html#a21146308d44a2de57aa21d12af7e5b65", null ],
+    [ "Stop", "_u___service_8hpp.html#a0532e721ee8f5d84f2c2f57cb3b19951", null ],
+    [ "SvcMain", "_u___service_8hpp.html#a2ef7fa664545ef1bfd08460f37d2140a", null ]
+];
