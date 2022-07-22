aefb7a8c0061b6eec85064fd5efc724758554a41
CapitaineHadd0ck <58261410+CapitaineHadd0ck@users.noreply.github.com>
2022-07-22 14:43:47 +0200

    Update U_GlobalHeader.hpp

--- Sources/PI-Defender_UM/U_GlobalHeader.hpp
+++ Sources/PI-Defender_UM/U_GlobalHeader.hpp
@@ -1,10 +1,12 @@
 /**
- * @file		U_GlobalHeader.hpp
- * @brief		Header used by several .hpp files
- * @author		Berenger BRAULT, Nicolas JALLET
- * @version		1.0
- * @date		02/06/2022
- * @copyright	LGPLv3
+ * @file       U_GlobalHeader.hpp
+ * @brief      eader used by several .hpp files
+ * @author     NAVAL-Group (Berenger BRAULT, Nicolas JALLET)
+ * @version    1.0
+ * @date       02/06/2022
+ * @copyright  ©Naval Group SA.
+ *             This document in its content and form is the property of Naval Group SA and/or third parties.
+ *             This project is released under the LGPLv3 license.
 */
 
 
