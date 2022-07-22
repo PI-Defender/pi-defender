ad5df86c50a976f4a6cf41d6998e7d371760ebba
CapitaineHadd0ck <58261410+CapitaineHadd0ck@users.noreply.github.com>
2022-07-22 15:02:28 +0200

    Update U_Signature.hpp

--- Sources/PI-Defender_UM/U_Signature.hpp
+++ Sources/PI-Defender_UM/U_Signature.hpp
@@ -1,10 +1,12 @@
 /**
- * @file		U_Signature.hpp
- * @brief		Header for U_Signature.cpp
- * @author		Berenger BRAULT, Nicolas JALLET
- * @version		1.0
- * @date		02/06/2022
- * @copyright	LGPLv3
+ * @file       U_Signature.hpp
+ * @brief      Header for U_Signature.cpp
+ * @author     NAVAL-Group (Berenger BRAULT, Nicolas JALLET)
+ * @version    1.0
+ * @date       02/06/2022
+ * @copyright  ©Naval Group SA.
+ *             This document in its content and form is the property of Naval Group SA and/or third parties.
+ *             This project is released under the LGPLv3 license.
 */
 
 
